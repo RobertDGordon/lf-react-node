e139e02c8b8dd701d5a65d4ccc43c6f9f9d57650
Robert Gordon <GordonRobertD@gmail.com>
2022-09-15 01:46:45 -0700

    Added post /api/submit  endpoint

--- server/api/server.js
+++ server/api/server.js
@@ -5,6 +5,8 @@ const server = express();
 const SUPERVISORS_URL = 'https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers';
 const test_data = require('./test_data.js');
 
+server.use(express.json());
+
 //Main route
 server.get('/api', (req, res) => {
     res.send(`<h2>Server is online!</h2><br><div>Visit: <a href='https://github.com/RobertDGordon/lf-react-node'>https://github.com/RobertDGordon/lf-react-node</a> for more API info.</div>`)
@@ -12,25 +14,36 @@ server.get('/api', (req, res) => {
 
 //Managers route
 server.get('/api/supervisors', async (req, res) => {
-    // let response = await axios(SUPERVISORS_URL);
-    
-    //remove numerical jurisdictions
-    let filtered = test_data.data.filter(item => !isFinite(item.jurisdiction))
+    try {
+        // let response = await axios(SUPERVISORS_URL);
+        let response = test_data;
 
-    //sort by jurisdiction, then lastName, then firstName (could be chained to above, but kept separate for readability)
-    let sorted = filtered.sort((a, b) => {
-        const jurisdiction = a.jurisdiction.localeCompare(b.jurisdiction);
-        const lastName = a.lastName.localeCompare(b.lastName);
-        const firstName = a.firstName.localeCompare(b.firstName);
+        //remove numerical jurisdictions
+        let filtered = response.data.filter(item => !isFinite(item.jurisdiction));
 
-        return jurisdiction || lastName || firstName
-    })
+        //sort by jurisdiction, then lastName, then firstName (could be chained to above, but kept separate for readability)
+        let sorted = filtered.sort((a, b) => {
+            const jurisdiction = a.jurisdiction.localeCompare(b.jurisdiction);
+            const lastName = a.lastName.localeCompare(b.lastName);
+            const firstName = a.firstName.localeCompare(b.firstName);
 
-    console.log(sorted)
+            return jurisdiction || lastName || firstName
+        });
 
-    res.status(200).json({response: sorted})
+        console.log(`Returning ${sorted.length} items.`);
 
-})
+        res.status(200).json({response: sorted});
 
+    } catch (error) {
+        console.log('Error:', error.message);
+        res.status(418).json({message: error.message})
+    }
+});
+
+//Post Notification route
+server.post('/api/submit', (req, res) => {
+    console.log(req.body)
+    res.status(201).json({response: "success"})
+});
 
 module.exports = server;
