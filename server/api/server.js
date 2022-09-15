5e140e138589dac230c8b8cf9f5239f594b2d9c2
Robert Gordon <GordonRobertD@gmail.com>
2022-09-14 23:37:36 -0700

    Added Axios, data recieved

--- server/api/server.js
+++ server/api/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const axios = require('axios');
 const server = express();
 
+const SUPERVISORS_URL = 'https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers';
+const test_data = require('./test_data.js');
+
+//Main route
 server.get('/api', (req, res) => {
     res.send(`<h2>Server is online!</h2><br><div>Visit: <a href='https://github.com/RobertDGordon/lf-react-node'>https://github.com/RobertDGordon/lf-react-node</a> for more API info.</div>`)
-    });
+});
+
+//Managers route
+server.get('/api/supervisors', async (req, res) => {
+    // let response = await axios(SUPERVISORS_URL);
+    let response = test_data
+
+    console.log(response.data)
+
+})
+
 
 module.exports = server;
